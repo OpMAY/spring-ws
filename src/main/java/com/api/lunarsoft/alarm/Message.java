2594ff0d7d4410b84be596abcbfda630a13aab8b
김우식 <zlzldntlr1@naver.com>
2022-07-27 04:45:44 +0900

    Lunarsoft Kakao Message API Complete

--- src/main/java/com/api/lunarsoft/alarm/Message.java
+++ src/main/java/com/api/lunarsoft/alarm/Message.java
@@ -1,10 +1,12 @@
 package com.api.lunarsoft.alarm;
 
-import lombok.Data;
+import lombok.*;
 
 import java.util.ArrayList;
 
-@Data
+@ToString
+@Getter
+@Setter
 public class Message {
     private String no;
     private String tel_num;
@@ -19,4 +21,21 @@ public class Message {
     private String use_sms = "1";
     private String app_user_id;
     private ArrayList<ButtonURL> btn_url;
+
+    @Builder
+    public Message(String no, String tel_num, String reserve_time, String custom_key, String msg_content, String sms_content, int price, String currency_code, String carrier_code, String invoice_number, String use_sms, String app_user_id, ArrayList<ButtonURL> btn_url) {
+        this.no = no;
+        this.tel_num = tel_num;
+        this.reserve_time = reserve_time;
+        this.custom_key = custom_key;
+        this.msg_content = msg_content;
+        this.sms_content = sms_content;
+        this.price = price;
+        this.currency_code = currency_code;
+        this.carrier_code = carrier_code;
+        this.invoice_number = invoice_number;
+        this.use_sms = use_sms;
+        this.app_user_id = app_user_id;
+        this.btn_url = btn_url;
+    }
 }
