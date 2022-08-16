95d7c27e21d2feb032cb673d621ee093f668f02d
김우식 <zlzldntlr1@naver.com>
2022-08-17 01:58:23 +0900

    update directory

---
+++ web/resources/js/plugin/sample.js
@@ -0,0 +1,34 @@
+/**
+ * Function Description,
+ * Test Function Call,
+ * Test Function Call2
+ *
+ * @param {param1} param1 Parameter Description
+ * @param {param2} param2 Parameter Description
+ * @return {String} Return Description
+ * */
+function test(param1, param2) {
+  return '';
+}
+
+/**
+ * @function test1
+ *
+ * @param {param1} param1 Parameter Description
+ * @param {param2} param2 Parameter Description
+ * @return {String} Return Description
+ * */
+function test1(param1, param2) {
+  return '';
+}
+
+/**
+ * @function test2
+ *
+ * @param {param1} param1 파라미터 상세
+ * @param {param2} param2 파라미터 상세
+ * @return {String} Return Description
+ * */
+function test2(param1, param2) {
+  return '';
+}
---
+++ web/resources/js/sample.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  console.log('module : library-sample.js execute');
+});
