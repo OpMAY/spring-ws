6e2ae0e6d639457672fcf6d20159758e8d67739e
김우식 <zlzldntlr1@naver.com>
2022-08-19 19:19:06 +0900

    response package update

--- src/main/java/com/response/DefaultRes.java
+++ src/main/java/com/response/DefaultRes.java
@@ -3,29 +3,30 @@ package com.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 
 @Data
 @AllArgsConstructor
 @Builder
 public class DefaultRes<T> {
 
-    private int status;
+    private HttpStatus status;
     private T data;
 
-    public static <T> DefaultRes<T> res(final int status) {
+    public static <T> DefaultRes<T> res(final HttpStatus status) {
         return DefaultRes.<T>builder()
                 .status(status)
                 .build();
     }
 
-    public static DefaultRes<Object> res(final int status, Message message) {
+    public static DefaultRes<Object> res(final HttpStatus status, Message message) {
         return DefaultRes.builder()
                 .data(message.getHashMap())
                 .status(status)
                 .build();
     }
 
-    public static DefaultRes<Object> res(final int status, Message message, boolean isLog) {
+    public static DefaultRes<Object> res(final HttpStatus status, Message message, boolean isLog) {
         return DefaultRes.builder()
                 .data(message.getHashMap(isLog))
                 .status(status)
--- src/main/java/com/response/ResMessage.java
+++
@@ -1,5 +0,0 @@
-package com.response;
-
-public class ResMessage {
-    public static final String TEST_SUCCESS = "The Test is successfully complete";
-}
